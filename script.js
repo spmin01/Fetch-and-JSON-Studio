0bc94c959dc596950741027c5c5494651d9d29bc
Shaun Minnihan <spminnihan@gmail.com>
2019-10-10 19:12:16 -0500

    first 2 bonuses complete

--- script.js
+++ script.js
@@ -2,6 +2,18 @@ window.addEventListener("load", function() {
     fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
         response.json().then(function(json) {
             let container = document.getElementById("container");
+
+            // sort ascending based on number of hours in space
+            json.sort(function(a, b) {
+                if (a.hoursInSpace > b.hoursInSpace) {
+                    return 1;
+                } else if (b.hoursInSpace > a.hoursInSpace) {
+                    return -1;
+                } else {
+                    return 0;
+                }
+            });
+
             for (let i = 0; i < json.length; i++) {
                 container.innerHTML += `
                     <div class="astronaut">
@@ -9,13 +21,14 @@ window.addEventListener("load", function() {
                             <h3>${json[i].firstName} ${json[i].lastName}</h3>
                             <ul>
                                 <li>Hours in space: ${json[i].hoursInSpace}</li>
-                                <li>Active: ${json[i].active}</li>
+                                <li style="color: ${json[i].active ? "green" : ""}">Active: ${json[i].active}</li>
                                 <li>Skills: ${json[i].skills.join(", ")}</li>
                             </ul>
                         </div>
-                        <img class="avatar" src="images/${json[i].picture} />
+                        <img class="avatar" src="${json[i].picture}" />
                     </div>
-                ;`
+                `;
+
             }
         });
     });
